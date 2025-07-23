bbb1637b4d3f6468f43fcf0add67273a721e5faa
saanvi-006 <saanvisrivastava2506@gmail.com>
2025-07-23 23:13:59 +0530

    Add files via upload

---
+++ insertElement.py
@@ -0,0 +1,5 @@
+def insert_element(arr,value):
+    arr.append(value)
+    return arr
+
+print(insert_element([1, 88, 35], 90)) 
---
+++ largestELement.py
@@ -0,0 +1,4 @@
+def largest_element(arr):
+    new_arr= arr.sort()
+    return arr[-1]
+print(largest_element([24,77,24,66,88,2]))
